d5d48893e4ae349869e10d6c8db15d6e192c5ccf
Alvianrml <alvianrmli@gmail.com>
2025-06-11 21:53:54 +0700

    upload project

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = FastAPI()
+
+# Load model Keras saat server start
+model = load_model("model.keras")
+
+# Sesuaikan input dengan fitur model kamu
+class InputData(BaseModel):
+    feature1: float
+    feature2: float
+    feature3: float
+
+@app.post("/predict")
+def predict(data: InputData):
+    input_array = np.array([[data.feature1, data.feature2, data.feature3]])
+    prediction = model.predict(input_array)
+    return {"prediction": prediction.tolist()}
