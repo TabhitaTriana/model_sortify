c3627f5ce399fec942705806d99b0e4d96938c20
Alvianrml <alvianrmli@gmail.com>
2025-06-11 23:26:57 +0700

    fix route model

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 from tensorflow.keras.models import load_model
+import os
+from pathlib import Path
 
 app = FastAPI()
 
 # Load model Keras saat server start
-model = load_model("model_klasifikasi_sampah.keras")
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "model_klasifikasi_sampah.keras"
 
+print(f"Looking for model at: {MODEL_PATH}")
+assert MODEL_PATH.exists(), "❌ Model file not found!"
+
+model = load_model(MODEL_PATH)
 # Sesuaikan input dengan fitur model kamu
 class InputData(BaseModel):
     feature1: float
