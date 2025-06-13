c7bac2dd9f5f45c0235895479b551b00157e012c
TabhitaTriana <bithhfp@gmail.com>
2025-06-13 08:55:16 +0700

    fix: use correct port parsing for railway

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Fallback ke 8000 jika $PORT tidak diset
+PORT=${PORT:-8000}
+
 echo "🕒 Waiting for model load..."
 sleep 2
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
+uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
